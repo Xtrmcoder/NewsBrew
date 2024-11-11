cedd2a76d4db75fa6b2a078da1708dea5a299c52
Xtrmcoder <mailme.pawaghmare@gmail.com>
2024-11-12 01:05:47 +0530

    done

--- src/components/News.js
+++ src/components/News.js
@@ -17,7 +17,7 @@ const News = (props) => {
     const updateNews = async () => {
         try {
             props.setProgress(10);
-            const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`; 
+            const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
             setLoading(true);
             let data = await fetch(url);
             props.setProgress(30);
